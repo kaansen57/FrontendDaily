a57fb7dfbd0b8f9fdd88dfca63fbd299127b3691
DESKTOP-7TS4NVF\kaans <senox57400@gmail.com>
2022-01-05 21:56:14 +0300

    mobile menu edited , mixins added

--- script.js
+++ script.js
@@ -4,38 +4,25 @@ let data = [
     title: "Cards",
     url: "/Day1/index.html",
   },
-  {
-    img: "img/1.png",
-    title: "Cards",
-    url: "/Day1/index.html",
-  },
-  {
-    img: "img/1.png",
-    title: "Cards",
-    url: "/Day1/index.html",
-  },
-  {
-    img: "img/1.png",
-    title: "Cards",
-    url: "/Day1/index.html",
-  },
-  {
-    img: "img/1.png",
-    title: "Cards",
-    url: "/Day1/index.html",
-  },
-  {
-    img: "img/1.png",
-    title: "Cards",
-    url: "/Day1/index.html",
-  },
 ];
 
 const cardSection = document.querySelector(".content");
+const mobileMenuButton = document.querySelector(".nav-icon");
+const mobileMenuItems = document.querySelector(".nav-desktop");
+cardData(data);
 
-for (let i = 0; i < data.length; i++) {
-    cardSection.innerHTML += 
-  `<div class="card">
+mobileMenuButton.addEventListener("click", () => {
+  if (mobileMenuItems.classList.value === "nav-desktop show") {
+        mobileMenuItems.className = "nav-desktop";
+  } else {
+    console.log(mobileMenuItems.classList);
+     mobileMenuItems.className = "nav-desktop show";
+  }
+});
+
+function cardData(data) {
+  for (let i = 0; i < data.length; i++) {
+    cardSection.innerHTML += `<div class="card">
      <div class="card-img">
         <img src="${data[i].img}" alt="${data[i].title}">
         <span class="card-number">${i + 1}</span>
@@ -44,8 +31,12 @@ for (let i = 0; i < data.length; i++) {
     <h5 class="card-title">${data[i].title}</h5>
     <hr>
 
-    <div class="card-footer">
-        <a href="${data[i].url}" target="_blank" class="btn btn-primary">View</a>
-    </div>
-  </div>`;
+     <div class="card-footer">
+        <a href="${
+          data[i].url
+        }" target="_blank" class="btn btn-primary">View</a>
+      </div>
+    </div>`;
+  }
+  return cardSection;
 }
